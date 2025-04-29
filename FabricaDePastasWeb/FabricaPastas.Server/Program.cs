99f3eea155bf41032e3dca081bc0cdb170e2da75
Leandro Bertero <leandro.bertero@hotmail.com>
2025-04-29 20:54:11 -0300

    Conexion string con la base de datos y el proyecto server, con los paquetes nuggets incluidos

--- FabricaDePastasWeb/FabricaPastas.Server/Program.cs
+++ FabricaDePastasWeb/FabricaPastas.Server/Program.cs
@@ -1,3 +1,6 @@
+using FabricaPastas.BD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region ConnectionString
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
+#endregion
 
 var app = builder.Build();
 
