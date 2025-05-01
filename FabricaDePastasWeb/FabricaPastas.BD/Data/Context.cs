bae2bcb1a058836be10cac3baace13a493668ae0
Leandro Bertero <leandro.bertero@hotmail.com>
2025-04-30 22:01:30 -0300

    OnModelCreating

--- FabricaDePastasWeb/FabricaPastas.BD/Data/Context.cs
+++ FabricaDePastasWeb/FabricaPastas.BD/Data/Context.cs
@@ -13,8 +13,20 @@ namespace FabricaPastas.BD.Data
         {
         }
 
-        protected Context()
+        #region OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Éste codigo sirve para evitar que se borren los datos en cascada en la base de datos
+            var cascadeFKs = modelBuilder.Model.G­etEntityTypes()
+                                            .SelectMany(t => t.GetForeignKeys())
+                                            .Where(fk => !fk.IsOwnership
+                                                        && fk.DeleteBehavior == DeleteBehavior.Casca­de);
+            foreach (var fk in cascadeFKs)
+            {        //Elimina el cmportamiento               
+                fk.DeleteBehavior = DeleteBehavior.Restr­ict;
+            }
         }
+        #endregion
+
     }
 }
